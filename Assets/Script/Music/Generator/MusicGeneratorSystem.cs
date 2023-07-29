8f6b0cc31fabaf0ff4affbdeb1964407770e05cd
weariness00 <ehddn917@gmail.com>
2023-07-30 00:24:53 +0900

    MusicGenerator - click 한 곳에 node가 생성
    물리 ray을 이용하여 노드 생성

--- Assets/Script/Music/Generator/MusicGeneratorSystem.cs
+++ Assets/Script/Music/Generator/MusicGeneratorSystem.cs
@@ -2,6 +2,7 @@
 using Unity.Burst;
 using Unity.Entities;
 using Unity.Mathematics;
+using Unity.Physics;
 using Unity.Transforms;
 using UnityEngine;
 
@@ -12,6 +13,7 @@ namespace Script.Music.Generator
         [BurstCompile]
         public void OnCreate(ref SystemState state)
         {
+            state.RequireForUpdate<PhysicsWorldSingleton>();
             state.RequireForUpdate<BeginSimulationEntityCommandBufferSystem.Singleton>();
             state.RequireForUpdate<MusicGeneratorTag>();
         }
@@ -30,27 +32,46 @@ namespace Script.Music.Generator
 
             if (Input.GetMouseButtonDown(0))
             {
-                float3 clickPosition = float3.zero;
-                Vector2 mousePositino = Input.mousePosition;
-                clickPosition = Camera.main.ScreenToWorldPoint();    
-                // clickPosition = 0;
+                var main = Camera.main;
+                Vector3 mousePosition = main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, main.nearClipPlane));
+                Vector3 cameraPosition = main.transform.position;
+                float3 clickDirection = mousePosition - cameraPosition;
 
-                foreach (var musicNodeAuthoring in SystemAPI.Query<MusicNodeAuthoring>())
+                var ray = new RaycastInput()
                 {
-                    if (clickPosition.Equals(musicNodeAuthoring.StartPosition))
+                    Start = cameraPosition,
+                    End = clickDirection * 1000f,
+                    Filter = new CollisionFilter
                     {
-                        return;
+                        GroupIndex = 0,
+                        // 1u << 6는 Physics Category Names에서 6번째의 레이어마스크이다.
+                        BelongsTo  = 1u << 0,
+                        CollidesWith = 1u << 6,
                     }
-                }
-
-                var newNodeEntity = ecb.Instantiate(generatorAspect.NodeEntities[0].Entity);
-                LocalTransform newNodeTransform = new LocalTransform()
-                {
-                    Position = clickPosition,
-                    Rotation = quaternion.identity,
-                    Scale = 1,
                 };
-                ecb.SetComponent(newNodeEntity, newNodeTransform);
+
+                PhysicsWorldSingleton physicsWorld = SystemAPI.GetSingleton<PhysicsWorldSingleton>();
+                if (physicsWorld.CastRay(ray, out var hit))
+                {
+                    float3 clickPosition = hit.Position;
+                    
+                    foreach (var musicNodeAuthoring in SystemAPI.Query<MusicNodeAuthoring>())
+                    {
+                        if (clickPosition.Equals(musicNodeAuthoring.StartPosition))
+                        {
+                            return;
+                        }
+                    }
+                    
+                    var newNodeEntity = ecb.Instantiate(generatorAspect.NodeEntities[0].Entity);
+                    LocalTransform newNodeTransform = new LocalTransform()
+                    {
+                        Position = clickPosition,
+                        Rotation = quaternion.identity,
+                        Scale = 1,
+                    };
+                    ecb.SetComponent(newNodeEntity, newNodeTransform);
+                }
             }
         }
     }
